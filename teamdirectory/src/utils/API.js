e69cf79e0fe3818b8f6bc91e4d3411a3bed6ab69
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-26 18:55:25 -0500

    updated API.js & index.js

---
+++ teamdirectory/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//export object containing methods used for accessing  the randomuser.me  API.
+
+export default {
+  getEmployees: function() {
+    return axios.get("https://randomuser.me/api/?results=250&inc=name,email,dob,phone,id,picture");
+  }
+  };
+
