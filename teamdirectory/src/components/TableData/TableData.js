d49b2d4138bd752321cfd027adf04b9e4df095bb
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-27 12:37:00 -0500

    formatted email

--- teamdirectory/src/components/TableData/TableData.js
+++ teamdirectory/src/components/TableData/TableData.js
@@ -10,7 +10,8 @@ function TableRow(props) {
         <img className="flex"src={props.picture} alt={props.firstname}></img></td>
         <td className="align-middle">{props.firstname} {props.lastname}</td>
         <td className="align-middle">{props.phone}</td>
-        <td className="align-middle">{props.email}</td>
+        <td className="align-middle">
+        <a href={props.email} target="__blank">{props.email}</a></td>
         <td className="align-middle">{props.DOB}</td>
       </tr>
     </tbody>
