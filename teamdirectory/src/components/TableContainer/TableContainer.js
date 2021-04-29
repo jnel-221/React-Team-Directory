011adb44d2c458625c8f7322cead9a5e9d4f5736
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-28 21:37:08 -0500

    updated API call, roughed out filtering function not yet filtering

--- teamdirectory/src/components/TableContainer/TableContainer.js
+++ teamdirectory/src/components/TableContainer/TableContainer.js
@@ -8,14 +8,30 @@ import API from "../../utils/API";
 class TableContainer extends Component {
   state = {
     result: [],
+    name:""
   };
 
-  componentDidMount(){
-      API.getEmployees().then(res=>this.setState({result: res.data.results})).catch(err=>console.log(err));
+  componentDidMount() {
+    API.getEmployees()
+      .then((res) => this.setState({ result: res.data.results }))
+      .catch((err) => console.log(err));
+  }
+
+  searchEmployees = () => {
+    const searchName = this.state.search.trim();
+    console.log(searchName);
+    // const searchResults= this.result.filter((employee)=> employee.firstname === searchName);
+    // this.setState({'result': searchResults})
+  }
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value,
+    });
   };
-
-
-
 
   render() {
     return (
@@ -23,10 +39,10 @@ class TableContainer extends Component {
         <Container />
         <Header />
         <NavSearch />
-        <div className="mx-4">
-        <TableHead results={this.state.result}/>
+        <div className="mx-5">
+          <TableHead results={this.state.result} />
         </div>
-    </>
+      </>
     );
   }
 }
